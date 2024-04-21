a89a88bd2dfb311bf847c6c80e1740a8be91cf2f
Harshit Vijay <harshitvj07@gmail.com>
2024-04-22 01:34:18 +0530

    Added Date Time Functionality in Appointment Booking (calendar & clock)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -239,12 +239,17 @@ class _DealersListPageState extends State<DealersListPage> {
   }
 }
 
-class DealerRatePage extends StatelessWidget {
+class DealerRatePage extends StatefulWidget {
   final String dealerId;
   final String username;
 
   DealerRatePage({required this.dealerId, required this.username});
 
+  @override
+  State<DealerRatePage> createState() => _DealerRatePageState();
+}
+
+class _DealerRatePageState extends State<DealerRatePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -254,7 +259,7 @@ class DealerRatePage extends StatelessWidget {
       body: FutureBuilder<DocumentSnapshot>(
         future: FirebaseFirestore.instance
             .collection('Kabadi_walas_ratelist')
-            .doc(dealerId)
+            .doc(widget.dealerId)
             .get(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -290,7 +295,7 @@ class DealerRatePage extends StatelessWidget {
                 child: ElevatedButton(
                   onPressed: () {
                     // Book appointment logic
-                    bookAppointment(context, dealerId, username);
+                    bookAppointment(context, widget.dealerId, widget.username);
                   },
                   child: Text("BOOK APPOINTMENT"),
                 ),
@@ -305,49 +310,139 @@ class DealerRatePage extends StatelessWidget {
   // Function to book appointment
   Future<void> bookAppointment(
       BuildContext context, String dealerId, String username) async {
-    try {
-      // Create a new appointment document
-      await FirebaseFirestore.instance.collection('Appointments').add({
-        'dealerId': dealerId, // ID of the dealer
-        'userName': username, // Username of the user
-        'timestamp':
-            FieldValue.serverTimestamp(), // Timestamp for the appointment
-      });
+    // DateTime? selectedDate;
+    DateTime? selectedDateTime;
+    // TimeOfDay? selectedTime;
 
-      // Show success message
-      showDialog(
+    // Function to show date picker
+    // Future<void> _selectDate() async {
+    Future<void> _selectDateTime() async {
+      // final DateTime? picked = await showDatePicker(
+      final DateTime? pickedDateTime = await showDatePicker(
         context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Appointment Booked'),
-          content: Text('Your appointment has been booked successfully!'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
-              },
-              child: Text('OK'),
-            ),
-          ],
-        ),
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2100),
       );
-    } catch (e) {
-      print('Error booking appointment: $e');
-      // Show error message
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Error'),
-          content: Text('Failed to book appointment. Please try again later.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
-              },
-              child: Text('OK'),
+
+      //   if (picked != null && picked != selectedDate) {
+      //     setState(() {
+      //       selectedDate = picked;
+      //     });
+      //   }
+      // }
+
+      // // Function to show time picker
+      // Future<void> _selectTime() async {
+      //   final TimeOfDay? picked = await showTimePicker(
+      //     context: context,
+      //     initialTime: TimeOfDay.now(),
+      //   );
+
+      //   if (picked != null && picked != selectedTime) {
+      //     setState(() {
+      //       selectedTime = picked;
+      //     });
+      //   }
+      // }
+      if (pickedDateTime != null) {
+        final TimeOfDay? pickedTime = await showTimePicker(
+          context: context,
+          initialTime: TimeOfDay.now(),
+        );
+
+        if (pickedTime != null) {
+          selectedDateTime = DateTime(
+            pickedDateTime.year,
+            pickedDateTime.month,
+            pickedDateTime.day,
+            pickedTime.hour,
+            pickedTime.minute,
+          );
+        }
+      }
+    }
+
+    // Show date and time picker
+    await _selectDateTime();
+
+    if (selectedDateTime != null) {
+      try {
+        // Check if the appointment already exists for the same user and date & time
+        QuerySnapshot<Map<String, dynamic>> existingAppointments =
+            await FirebaseFirestore.instance
+                .collection('Appointments')
+                .where('dealerId', isEqualTo: dealerId)
+                .where('userName', isEqualTo: username)
+                .where('appointmentDateTime', isEqualTo: selectedDateTime)
+                .get();
+
+        if (existingAppointments.docs.isEmpty) {
+          // Create a new appointment document
+          await FirebaseFirestore.instance.collection('Appointments').add({
+            'dealerId': dealerId, // ID of the dealer
+            'userName': username, // Username of the user
+            'appointmentDateTime':
+                selectedDateTime, // Date & time of the appointment
+            'timestamp':
+                FieldValue.serverTimestamp(), // Timestamp for the appointment
+          });
+
+          // Show success message
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text('Appointment Booked'),
+              content: Text('Your appointment has been booked successfully!'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Close the dialog
+                  },
+                  child: Text('OK'),
+                ),
+              ],
             ),
-          ],
-        ),
-      );
+          );
+        } else {
+          // Show error message if appointment already exists
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text('Appointment Already Booked'),
+              content: Text(
+                  'An appointment already exists for the same user and date & time.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Close the dialog
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            ),
+          );
+        }
+      } catch (e) {
+        print('Error booking appointment: $e');
+        // Show error message
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Error'),
+            content:
+                Text('Failed to book appointment. Please try again later.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // Close the dialog
+                },
+                child: Text('OK'),
+              ),
+            ],
+          ),
+        );
+      }
     }
   }
 }
